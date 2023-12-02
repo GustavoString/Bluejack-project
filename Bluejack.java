3581f6a0ce7020ede78b72d2e8fb1c7e0a5c0c74
GustavoString <william@allamand.co.uk>
2023-12-03 00:07:07 +0300

    Continued to work on my SE 115 project
    Added a scanner to the main so I don't forget later, added a player deck generator function and added a game deck generator function.
    Also generated and shuffled the game deck in main then generated the player deck.

--- Bluejack.java
+++ Bluejack.java
@@ -1,7 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Bluejack{
 
     public static void main(String[] args) {
-        
+        Scanner scan1=new Scanner(System.in);
+        CardDeck[] gamedeck=new CardDeck[40];
+        gamedeck=CardDeck.generateGameDeck();
+        gamedeck=CardDeck.shuffle(gamedeck);
+        CardDeck[] playerdeck=playerDeckGenerator(gamedeck);
+    }
+
+    public static CardDeck[] playerDeckGenerator(CardDeck[] gamedeck){
+        Random r1=new Random(System.currentTimeMillis());
+        CardDeck[] playerdeck=new CardDeck[10];
+        int j=0;
+        for (int i = gamedeck.length-1; i>gamedeck.length-6  ; i--) {
+            playerdeck[j].CardType=1;
+            playerdeck[j].CardColour=gamedeck[i].CardColour;
+            playerdeck[j].CardValue=gamedeck[i].CardValue;
+            j++;
+        }
+        for (int i=j ;  i< playerdeck.length-2 ; i++) {
+            j++;
+            int signRandom=r1.nextInt(2);
+            int valueRandom=r1.nextInt(1,7);
+            int colourRandom=r1.nextInt(1,5);
+            playerdeck[i].CardColour=colourRandom;
+            playerdeck[i].CardType=1;
+            if(signRandom==1) playerdeck[i].CardValue=valueRandom;
+            else playerdeck[i].CardValue=-1*valueRandom;
+        }
+        for (int i = j; i < playerdeck.length ; i++) {
+            int percentRandom=r1.nextInt(1,11);
+            if(percentRandom<=2){
+                int coinflip=r1.nextInt(1,3);
+                if(coinflip==1) playerdeck[i].CardType=2;
+                else playerdeck[i].CardType=3;
+            }
+            else{
+                int signRandom=r1.nextInt(2);
+                int valueRandom=r1.nextInt(1,7);
+                int colourRandom=r1.nextInt(1,5);
+                playerdeck[i].CardColour=colourRandom;
+                playerdeck[i].CardType=1;
+                if(signRandom==1) playerdeck[i].CardValue=valueRandom;
+                else playerdeck[i].CardValue=-1*valueRandom;
+            }
+        }
+        //randomly selecting 4 after this point
+        CardDeck[] playerdeckreturn=new CardDeck[4];
+        int a=10;
+        int b=1;
+        for (int i = 0; i < playerdeckreturn.length; i++) {
+            int randomselector=r1.nextInt(a);
+            playerdeckreturn[i]=playerdeck[randomselector];
+            a--;
+            for (int k = randomselector; k < playerdeck.length-b; k++) {
+                playerdeck[k]=playerdeck[k+1];
+            }
+            b++;
+        }
+        return playerdeckreturn;
     }
 }
--- CardDeck.java
+++ CardDeck.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class CardDeck {
     int CardType;
-    //signed=1, flip=2, double=3 
+    //signed=1, flip=2, double=3, no card=0
     int CardColour;
     //blue=1, green=2, red=3, yellow=4
     int CardValue;
@@ -11,6 +11,11 @@ public class CardDeck {
     public CardDeck(int type){
         //constructor for flip and double cards
         CardType=type;
+    }
+
+    public CardDeck(int type, int colour){
+        CardType=type;
+        CardColour=colour;
     }
 
     public CardDeck(int type, int colour, int value){
@@ -32,5 +37,29 @@ public class CardDeck {
             }
         }
         return shuffled;
+    }
+
+    public static CardDeck[] generateGameDeck() {
+        CardDeck[] GameDeck=new CardDeck[40];
+        for (int i = 0; i < GameDeck.length; i++) {
+            GameDeck[i].CardType=1;
+            if(i<10){
+                GameDeck[i].CardColour=1;
+                GameDeck[i].CardValue=i+1;
+            }
+            else if(i<20){
+                GameDeck[i].CardColour=2;
+                GameDeck[i].CardValue=i-9;
+            }
+            else if(i<30){
+                GameDeck[i].CardColour=3;
+                GameDeck[i].CardValue=i-19;
+            }
+            else{
+                GameDeck[i].CardColour=4;
+                GameDeck[i].CardValue=i-29;
+            }
+        }
+        return GameDeck;
     }
 }
