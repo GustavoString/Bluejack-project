c1de8970d6fd78be03c4b102d469fbd8675d9a8d
GustavoString <william@allamand.co.uk>
2023-12-07 22:17:04 +0300

    Noticed a possible problem to do with the deck generator functions and fixed it

--- Bluejack.java
+++ Bluejack.java
@@ -4,127 +4,137 @@ import java.util.Scanner;
 public class Bluejack{
 
     public static void main(String[] args) {
+        Random r1=new Random(System.currentTimeMillis());
         Scanner scan1=new Scanner(System.in);
         //creating the game deck
         CardDeck[] gamedeck=new CardDeck[40];
         gamedeck=CardDeck.generateGameDeck();
         gamedeck=CardDeck.shuffle(gamedeck);
         //creating the player deck (hand)
-        CardDeck[] playerdeck=playerDeckGenerator(gamedeck);
-        //removing the bottom 5 cards as they have been given to the user
-        for (int i = gamedeck.length-1; i > gamedeck.length-6; i--) {
-            gamedeck[i].CardType=0;
-        }
-        //creating bot deck
-        CardDeck[] botdeck=botDeckGenerator(gamedeck);
-        //removing top 5 cards in gamedeck and shifting the cards
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < gamedeck.length-1; j++) {
-                gamedeck[j].CardType=gamedeck[j+1].CardType;
-            }
-        }
-        
-    }
-
-    public static CardDeck[] playerDeckGenerator(CardDeck[] gamedeck){
-        Random r1=new Random(System.currentTimeMillis());
-        CardDeck[] playerdeck=new CardDeck[10];
+        //ge erating player deck after this point
+        CardDeck[] playerdeck=new CardDeck[4];
         int j=0;
+        CardDeck[] playerdeckUnchosen=new CardDeck[10];
         for (int i = gamedeck.length-1; i>gamedeck.length-6  ; i--) {
-            playerdeck[j].CardType=1;
-            playerdeck[j].CardColour=gamedeck[i].CardColour;
-            playerdeck[j].CardValue=gamedeck[i].CardValue;
+            playerdeckUnchosen[j]=gamedeck[i];
             j++;
         }
-        for (int i=j ;  i< playerdeck.length-2 ; i++) {
+        for (int i=j ;  i< playerdeckUnchosen.length-2 ; i++) {
             j++;
             int signRandom=r1.nextInt(2);
             int valueRandom=r1.nextInt(1,7);
             int colourRandom=r1.nextInt(1,5);
-            playerdeck[i].CardColour=colourRandom;
-            playerdeck[i].CardType=1;
-            if(signRandom==1) playerdeck[i].CardValue=valueRandom;
-            else playerdeck[i].CardValue=-1*valueRandom;
+            playerdeckUnchosen[i].CardColour=colourRandom;
+            playerdeckUnchosen[i].CardType=1;
+            if(signRandom==1) playerdeckUnchosen[i].CardValue=valueRandom;
+            else playerdeckUnchosen[i].CardValue=-1*valueRandom;
         }
-        for (int i = j; i < playerdeck.length ; i++) {
+        for (int i = j; i < playerdeckUnchosen.length ; i++) {
             int percentRandom=r1.nextInt(1,11);
             if(percentRandom<=2){
                 int coinflip=r1.nextInt(1,3);
-                if(coinflip==1) playerdeck[i].CardType=2;
-                else playerdeck[i].CardType=3;
+                if(coinflip==1) playerdeckUnchosen[i].CardType=2;
+                else playerdeckUnchosen[i].CardType=3;
             }
             else{
                 int signRandom=r1.nextInt(2);
                 int valueRandom=r1.nextInt(1,7);
                 int colourRandom=r1.nextInt(1,5);
-                playerdeck[i].CardColour=colourRandom;
-                playerdeck[i].CardType=1;
-                if(signRandom==1) playerdeck[i].CardValue=valueRandom;
-                else playerdeck[i].CardValue=-1*valueRandom;
+                playerdeckUnchosen[i].CardColour=colourRandom;
+                playerdeckUnchosen[i].CardType=1;
+                if(signRandom==1) playerdeckUnchosen[i].CardValue=valueRandom;
+                else playerdeckUnchosen[i].CardValue=-1*valueRandom;
             }
         }
-        //randomly selecting 4 after this point
-        CardDeck[] playerdeckreturn=new CardDeck[4];
-        int a=10;
-        int b=1;
-        for (int i = 0; i < playerdeckreturn.length; i++) {
-            int randomselector=r1.nextInt(a);
-            playerdeckreturn[i]=playerdeck[randomselector];
-            a--;
-            for (int k = randomselector; k < playerdeck.length-b; k++) {
-                playerdeck[k]=playerdeck[k+1];
+        int[] randomselectors=new int[4];
+        for (int i = 0; i < randomselectors.length; i++) {
+            randomselectors[i]=r1.nextInt(10);
+            if(i>0){
+                for (int k = 0; k < i; k++) {
+                    while(randomselectors[k]==randomselectors[i]){
+                        randomselectors[k]=r1.nextInt(10);
+                    }
+                }
             }
-            b++;
         }
-        return playerdeckreturn;
-    }
-
-    public static CardDeck[] botDeckGenerator(CardDeck[] gamedeck){
-        Random r1=new Random(System.currentTimeMillis());
-        CardDeck[] botdeck=new CardDeck[10];
-        for (int i = 0; i<5  ; i++) {
-            botdeck[i].CardType=1;
-            botdeck[i].CardColour=gamedeck[i].CardColour;
-            botdeck[i].CardValue=gamedeck[i].CardValue;
+        for (int i = 0; i < randomselectors.length; i++) {
+            playerdeck[i]=playerdeckUnchosen[randomselectors[i]];
         }
-        for (int i=5 ;  i< botdeck.length-2 ; i++) {
+        for (int i = 0; i < randomselectors.length; i++) {
+            if(randomselectors[i]<=5){
+                gamedeck[gamedeck.length-1-i].CardType=0;
+            }
+        }
+        for (int i = 0; i < gamedeck.length-1; i++) {
+            if(gamedeck[i].CardType==0) {
+                gamedeck[i]=gamedeck[i+1];
+            }
+        }
+        //IMPORTANT!! after this point you removed the top and bottom 5 cards but that is wrong as they all may not have been given to the user/bot.
+        //write something that figures out which ones have been given to the user / bot and remove ONLY those.
+        
+        //creating bot deck
+        CardDeck[] botdeck=new CardDeck[4];
+        CardDeck[] botdeckUnchosen=new CardDeck[10];
+        //do the same fix for bot deck aswell
+        for (j = 0; j < 5; j++) {
+            botdeckUnchosen[j]=gamedeck[j];
+        }
+        for (int i=j ;  i< botdeckUnchosen.length-2 ; i++) {
+            j++;
             int signRandom=r1.nextInt(2);
             int valueRandom=r1.nextInt(1,7);
             int colourRandom=r1.nextInt(1,5);
-            botdeck[i].CardColour=colourRandom;
-            botdeck[i].CardType=1;
-            if(signRandom==1) botdeck[i].CardValue=valueRandom;
-            else botdeck[i].CardValue=-1*valueRandom;
+            botdeckUnchosen[i].CardColour=colourRandom;
+            botdeckUnchosen[i].CardType=1;
+            if(signRandom==1) botdeckUnchosen[i].CardValue=valueRandom;
+            else botdeckUnchosen[i].CardValue=-1*valueRandom;
         }
-        for (int i = botdeck.length-2; i < botdeck.length ; i++) {
+        for (int i = j; i < botdeckUnchosen.length ; i++) {
             int percentRandom=r1.nextInt(1,11);
             if(percentRandom<=2){
                 int coinflip=r1.nextInt(1,3);
-                if(coinflip==1) botdeck[i].CardType=2;
-                else botdeck[i].CardType=3;
+                if(coinflip==1) botdeckUnchosen[i].CardType=2;
+                else botdeckUnchosen[i].CardType=3;
             }
             else{
                 int signRandom=r1.nextInt(2);
                 int valueRandom=r1.nextInt(1,7);
                 int colourRandom=r1.nextInt(1,5);
-                botdeck[i].CardColour=colourRandom;
-                botdeck[i].CardType=1;
-                if(signRandom==1) botdeck[i].CardValue=valueRandom;
-                else botdeck[i].CardValue=-1*valueRandom;
+                botdeckUnchosen[i].CardColour=colourRandom;
+                botdeckUnchosen[i].CardType=1;
+                if(signRandom==1) botdeckUnchosen[i].CardValue=valueRandom;
+                else botdeckUnchosen[i].CardValue=-1*valueRandom;
             }
         }
-        CardDeck[] botdeckreturn=new CardDeck[4];
-        int a=10;
-        int b=1;
-        for (int i = 0; i < botdeckreturn.length; i++) {
-            int randomselector=r1.nextInt(a);
-            botdeckreturn[i]=botdeck[randomselector];
-            a--;
-            for (int k = randomselector; k < botdeck.length-b; k++) {
-                botdeck[k]=botdeck[k+1];
-            }
-            b++;
+        for (int i = 0; i < randomselectors.length; i++) {
+            randomselectors[i]=-1;
         }
-        return botdeckreturn;
+        for (int i = 0; i < randomselectors.length; i++) {
+            randomselectors[i]=r1.nextInt(10);
+            if(i>0){
+                for (int k = 0; k < i; k++) {
+                    while(randomselectors[k]==randomselectors[i]){
+                        randomselectors[k]=r1.nextInt(10);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < randomselectors.length; i++) {
+            botdeck[i]=botdeckUnchosen[randomselectors[i]];
+        }
+        for (int i = 0; i < randomselectors.length; i++) {
+            if(randomselectors[i]<=5){
+                gamedeck[i].CardType=0;
+            }
+        }
+        for (int i = 0; i < gamedeck.length-1; i++) {
+            if(gamedeck[i].CardType==0) {
+                gamedeck[i]=gamedeck[i+1];
+            }
+        }
+        //finished creating player hands at this point
     }
+    
+    
 }
